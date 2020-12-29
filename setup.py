c47f331823ebeb4e3fd0a0085801e842e7124ec9
Renaud Gaspard <gaspardrenaud@hotmail.com>
2020-12-29 12:32:39 +0100

    corrected typo in version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def get_long_description():
 
 setuptools.setup(
     name="pyng",
-    version="0.1.0r",
+    version="0.1.0",
     author="Renaud Gaspard",
     author_email="gaspardrenaud@hotmail.com",
     description="Command utility to ping multiple hosts at once",
